dea9402bb03f1200951b79c6c73d410f0416650b
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-01-05 07:48:31 +0800

    Improve

--- publish/gatewayworker.php
+++ publish/gatewayworker.php
@@ -9,12 +9,14 @@ declare(strict_types=1);
  * @contact  huangdijia@gmail.com
  */
 return [
+    'register_address' => env('GATEWAYWORKER_REGISTER_ADDRESS', '127.0.0.1:1215'),
+
     'register' => [
         'bind' => env('GATEWAYWORKER_REGISTER_BIND', '0.0.0.0:1215'),
         'name' => env('GATEWAYWORKER_REGISTER_NAME', 'Register'),
         'processes' => env('GATEWAYWORKER_REGISTER_PROCESSES', 1),
     ],
-    'register_address' => env('GATEWAYWORKER_REGISTER_ADDRESS', '127.0.0.1:1215'),
+
     'gateway' => [
         'bind' => env('GATEWAYWORKER_GATEWAY_BIND', '0.0.0.0:1216'),
         'name' => env('GATEWAYWORKER_GATEWAY_NAME', 'Gateway'),
@@ -24,10 +26,12 @@ return [
         'ping_data' => env('GATEWAYWORKER_GATEWAY_PING_DATA', '{"mode":"heart"}'),
         'ping_not_response_limit' => env('GATEWAYWORKER_GATEWAY_PING_NOT_RESPONSE_LIMIT', 0),
     ],
+
     'businessworker' => [
         'name' => env('GATEWAYWORKER_BUSINESSWORKER_NAME', 'BusinessWorker'),
         'processes' => env('GATEWAYWORKER_BUSINESSWORKER_PROCESSES', 1),
-        'event_handler' => '', // implements FriendsOfHyperf\GatewayWorker\GatewayWorkerEventInterface
+        // event_handler must implements FriendsOfHyperf\GatewayWorker\GatewayWorkerEventInterface
+        'event_handler' => '',
     ],
 
     'event_loop_class' => \Workerman\Events\Swow::class,
--- src/Client.php
+++ src/Client.php
@@ -11,7 +11,9 @@ declare(strict_types=1);
 namespace FriendsOfHyperf\GatewayWorker;
 
 use GatewayClient\Gateway;
+use Hyperf\Macroable\Macroable;
 
 class Client extends Gateway
 {
+    use Macroable;
 }
